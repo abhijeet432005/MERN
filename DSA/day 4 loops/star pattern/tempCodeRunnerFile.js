81bd259bf1c2588e5e69bc2caa30f2c049ad3d46
abhijeet432005 <abhijeet42kumar@gmail.com>
2025-03-18 21:29:46 +0530

    .

---
+++ DSA/day 4 loops/star pattern/tempCodeRunnerFile.js
@@ -0,0 +1,13 @@
+let n = prompt("Enter the number ");
+
+for(let i = 1; i <= n; i++){
+
+    for(let j = 1; j <= n - i; j++){
+        process.stdout.write(" ");
+    }
+    for(let k = 1; k <= i; k++){
+        process.stdout.write("*");
+
+    }
+    console.log();
+}
