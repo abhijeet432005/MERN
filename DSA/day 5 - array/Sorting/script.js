0848a3ff1a93a2c400e1ecb691b4a3dec6a80837
abhijeet432005 <abhijeet42kumar@gmail.com>
2025-04-01 20:59:53 +0530

    .

--- DSA/day 5 - array/Sorting/script.js
+++ DSA/day 5 - array/Sorting/script.js
@@ -1,17 +1,48 @@
 let prompt = require("prompt-sync")()
 
+
+// 1. Bubble sort 
+
+// let arr = [1,4,3,2,9,10];
+
+// for(let i = 0; i < arr.length - 1; i++){
+
+//     for(let j = 0; j < arr.length - i - 1; j++){
+
+//         if(arr[j] > arr[j + 1]){
+//             let temp = arr[j];
+//             arr[j] = arr[j + 1];
+//             arr[j + 1] = temp;
+//         }
+//     }
+    
+// }
+// console.log(arr)
+
+
+// *******************************************************************************************************
+// *******************************************************************************************************
+// *******************************************************************************************************
+
+
+// 2. Selection Sort
+
 let arr = [1,4,3,2,9,10];
 
 for(let i = 0; i < arr.length - 1; i++){
+    let small = i;
 
-    for(let j = 0; j < arr.length - i - 1; j++){
-
-        if(arr[j] > arr[j + 1]){
-            let temp = arr[j];
-            arr[j] = arr[j + 1];
-            arr[j + 1] = temp;
+    for(let j = i + 1; i < arr.length; j++){
+        if(arr[small] > arr[j]){
+            small = j;
         }
     }
-    
+
+    if(i != small){
+        let temp = arr[i];
+        arr[i] = arr[small];
+        arr[small] = temp;
+    }
 }
-console.log(arr)
+
+console.log(arr);
