db013c9c9d089a207672dbbeeeb6377bce3e6fc5
abhijeet432005 <abhijeet42kumar@gmail.com>
2025-04-17 15:32:02 +0530

    .

--- JavaScript/js 6 - closure , hofs/function/script.js
+++ JavaScript/js 6 - closure , hofs/function/script.js
@@ -1,22 +1,15 @@
-// let greet = name => {
-//     return `Hello ${name}`;
+// 1.
 
+// function abcd(cb){
+//     setTimeout(() => {
+//         cb()    
+//     }, 3000);
 // }
 
-// console.log(greet("Alex"))
+// abcd(function(){
+//     console.log('hii');
+// })
 
 
-// function abcd(a, b, c, ...rest){
-//     console.log(a, b, c, rest);
-// }
 
-// abcd(1,2,3,4,5,6,7,8,9)
-
-
-// console.log(a)
-// var a = 12
-
-
-(function abcd(a, b){
-    console.log(a + b)
-})(2, 5);
+// 2.
