a4882c0c582d1839d98329b922b862854194275c
abhijeet432005 <abhijeet42kumar@gmail.com>
2025-04-17 21:24:40 +0530

    .

--- JavaScript/js 6 - closure , hofs/function/script.js
+++ JavaScript/js 6 - closure , hofs/function/script.js
@@ -1,8 +1,8 @@
 // 1.
 
-// function abcd(cb){
+// function abcd(fn){
 //     setTimeout(() => {
-//         cb()    
+//         fn();
 //     }, 3000);
 // }
 
@@ -13,3 +13,108 @@
 
 
 // 2.
+
+// let arr = [1,2,3,4,5,6];
+
+// function hap(array, fn){
+//     let newarray = [];
+    
+//     for(let i = 0; i < arr.length; i++){
+//         newarray.push(fn(array[i]))
+//     }
+
+//     return newarray;
+// }
+
+// console.log(hap(arr, function(value){
+//     return value + 3;
+// }))
+
+
+// 3.
+
+// function count(){
+//     let count = 0;
+//     return () => {
+//         count++;
+//         console.log(count)
+//     }
+// }
+
+// let inc =  count();
+// inc()
+// inc()
+// inc()
+
+
+
+// 4.
+
+
+// function limiter(fn, limit){
+//     let total = 0;
+
+//     return () => {
+//         if(total < limit){
+//             fn();
+//             total++;
+//         }
+//         else console.log('aukkat me rhe')
+//     }
+// }
+
+// let ans =  limiter(function(){
+//     console.log('hey')
+// }, 3);
+
+// ans();
+// ans();
+// ans();
+
+
+// 5. 
+
+// function cb(fn){
+//     setInterval(() => {
+//        fn() 
+//     }, 2000);
+// }
+
+// cb(() => {
+//     console.log('hii')
+// })
+
+
+// 6.
+
+// function greet(greeting){
+
+//     return (name) => {
+//         console.log(`${greeting} ${name}`);
+//     }
+// }
+
+// let ans = greet('Hello');
+// ans('Chota Bhim')
+
+
+
+// 7.
+
+function throttle(fn, Delay){
+    let lastcall = 0;
+    return () => {
+        let current = Date.now();
+        if(current - lastcall >= Delay){
+            lastcall = current;
+            fn()
+        }
+    }
+}
+
+let ans = throttle(() => {
+    console.log('hii')
+}, 2000)
+
+ans()
+ans() 
