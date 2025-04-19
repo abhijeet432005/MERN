9ddf86d0a81c9f64b7e04dd65edbccc52f086bb3
abhijeet432005 <abhijeet42kumar@gmail.com>
2025-04-19 22:50:41 +0530

    .

--- project/sundown studio/script.js
+++ project/sundown studio/script.js
@@ -4,3 +4,79 @@ const scroll = new LocomotiveScroll({
 });
 
 
+function swiperanim(){
+    var swiper = new Swiper(".mySwiper", {
+        slidesPerView: "auto",
+        centeredSlides: true,
+        spaceBetween: 30,
+        
+      });
+
+}
+
+swiperanim()
+
+
+
+function imageeffect(){
+    let elem = document.querySelectorAll('.sec');
+
+
+console.log(elem)
+
+
+elem.forEach(function(val){
+
+   
+
+    val.addEventListener('mouseenter', function(dets){
+        val.childNodes[3].style.opacity = 1
+        
+    })
+    val.addEventListener('mouseleave', function(dets){
+        val.childNodes[3].style.opacity = 0
+    })
+})
+}
+
+if(window.innerWidth >= 1000){
+    imageeffect()
+
+}
+
+
+
+function menuanim(){
+    let menu = document.querySelector('nav .menu')
+    let full = document.querySelector('.full-scr');
+    let navimg = document.querySelector('nav img')
+    let flag = 0;
+    
+    menu.addEventListener('click', function(){
+        if(flag === 0){
+            full.style.top = 0
+            navimg.style.opacity = 0
+            flag = 1
+        }
+        
+        else{
+            full.style.top = '-100%'
+            navimg.style.opacity = 1
+            flag = 0
+    
+        }
+    })
+
+}
+
+menuanim()
+
+function loader(){
+    let load = document.querySelector('.loader')
+    
+    setTimeout(function(){
+        load.style.top = '-100%'
+    }, 4000)
+}
+
+loader();
