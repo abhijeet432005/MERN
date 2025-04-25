c0f8c0113e7f02071ed7baa060ec41e0dd33bab8
abhijeet432005 <abhijeet42kumar@gmail.com>
2025-04-25 20:53:54 +0530

    .

--- CSS/fylla responsive/script.js
+++ CSS/fylla responsive/script.js
@@ -1,5 +1,17 @@
-const scroll = new LocomotiveScroll({
-    el: document.querySelector('main'),
-    smooth: true
-});
 
+
+let crsr = document.querySelector('.cursor');
+let crsr2 = document.querySelector('.mini');
+
+document.addEventListener('mousemove', function(dets){
+
+    setTimeout(() => {
+        crsr.style.opacity = 1
+        crsr2.style.opacity = 1
+    }, 100);
+
+    crsr.style.top = dets.clientY+'px'
+    crsr.style.left = dets.clientX+'px'
+    crsr2.style.top = dets.clientY+'px'
+    crsr2.style.left = dets.clientX+'px'
+})
