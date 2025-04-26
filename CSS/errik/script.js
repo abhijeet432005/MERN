12e8544e1dda06221ecbfd5194bc8aa66d7d45b9
abhijeet432005 <abhijeet42kumar@gmail.com>
2025-04-26 17:44:20 +0530

    .

--- CSS/errik/script.js
+++ CSS/errik/script.js
@@ -2,7 +2,7 @@ let main = document.querySelector('main');
 let menu = document.querySelector('.menu i');
 let nav = document.querySelector('.nav');
 let closed = document.querySelector('.close i')
-let flag = 0;
+
 
 menu.addEventListener('click', function(){
     nav.style.top = '0';
@@ -11,4 +11,20 @@ menu.addEventListener('click', function(){
 closed.addEventListener('click', function(){
     nav.style.top = '-100%';
 
+})
+
+let crsr = document.querySelector('.cursor');
+let mini = document.querySelector('.mini');
+
+document.addEventListener('mousemove', function(dets){
+
+    setTimeout(() => {
+        mini.style.opacity = 1
+        crsr.style.opacity = 1
+    }, 100);
+
+    crsr.style.top = dets.clientY+'px'
+    crsr.style.left = dets.clientX+'px'
+    mini.style.top = dets.clientY+'px'
+    mini.style.left = dets.clientX+'px'
 })
