7bec090f5da71a87e140882694c411e2539ac165
abhijeet432005 <abhijeet42kumar@gmail.com>
2025-04-28 21:40:53 +0530

    .

---
+++ CSS/beaute/script.js
@@ -0,0 +1,27 @@
+Shery.imageEffect(".image", {
+    style: 3,
+    /*optional parameters
+    these parameter dose not applies to custom scroll trigger callback */
+    scrollSnapping: true,
+    scrollSpeed: 4,
+    touchSpeed: 8,
+    damping: 7,
+    config: {"uFrequencyX":{"value":37.4,"range":[0,100]},"uFrequencyY":{"value":25.19,"range":[0,100]},"uFrequencyZ":{"value":15.27,"range":[0,100]},"geoVertex":{"range":[1,64],"value":32},"zindex":{"value":-9996999,"range":[-9999999,9999999]},"aspect":{"value":1.0446136932105115},"ignoreShapeAspect":{"value":true},"shapePosition":{"value":{"x":-0.03,"y":0}},"shapeScale":{"value":{"x":0.32,"y":0.46}},"shapeEdgeSoftness":{"value":0,"range":[0,0.5]},"shapeRadius":{"value":0.03,"range":[0,2]},"currentScroll":{"value":2.9999999999999973},"scrollLerp":{"value":0.07},"gooey":{"value":false},"infiniteGooey":{"value":false},"growSize":{"value":4,"range":[1,15]},"durationOut":{"value":1,"range":[0.1,5]},"durationIn":{"value":1.5,"range":[0.1,5]},"displaceAmount":{"value":0.5},"masker":{"value":true},"maskVal":{"value":1.09,"range":[1,5]},"scrollType":{"value":1},"noEffectGooey":{"value":true},"onMouse":{"value":1},"noise_speed":{"value":0.2,"range":[0,10]},"metaball":{"value":0.2,"range":[0,2]},"discard_threshold":{"value":0.5,"range":[0,1]},"antialias_threshold":{"value":0.002,"range":[0,0.1]},"noise_height":{"value":0.5,"range":[0,2]},"noise_scale":{"value":10,"range":[0,100]}},
+  });
+
+  Shery.mouseFollower({
+    //Parameters are optional.
+    skew: true,
+    ease: "cubic-bezier(0.23, 1, 0.320, 1)",
+    duration: 1,
+  });
+
+  Shery.textAnimate(".text" /* Element to target.*/, {
+    //Parameters are optional.
+    style: 1,
+    y: 10,
+    delay: 0.1,
+    duration: 2,
+    ease: "cubic-bezier(0.23, 1, 0.320, 1)",
+    multiplier: 0.1,
+  });
