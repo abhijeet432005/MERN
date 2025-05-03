f4b08f23f5b09a4f89fc776d078bd352b093c779
abhijeet432005 <abhijeet42kumar@gmail.com>
2025-05-03 21:11:29 +0530

    .

--- DOM Manipulation/image hover effect/card stack animation/script.js
+++ DOM Manipulation/image hover effect/card stack animation/script.js
@@ -1,43 +1,108 @@
-const blocks = document.querySelectorAll('.block');
-const reset = 300
 
-blocks.forEach((block) => {
-    let timeout;
+const position = [
+    {top: "0%", left: "0%"},
+    {top: "0%", left: "10%"},
+    {top: "0%", left: "60%"},
+    {top: "16%", left: "15%"},
+    {top: "16%", left: "40%"},
+    {top: "16%", left: "90%"},
+    {top: "32%", left: "50%"},
+    {top: "32%", left: "75%"},
+    {top: "48%", left: "0%"},
+    {top: "64%", left: "30%"},
+    {top: "64%", left: "50%"},
+    {top: "64%", left: "90%"},
+    {top: "80%", left: "20%"},
+    {top: "80%", left: "70%"},
+];
 
-    block.addEventListener('mousemove', () => {
-        clearTimeout(timeout);
-        block.classList.add("active");
+const imgs = document.querySelectorAll(".img");
 
-        timeout = setTimeout(() => {
-            block.classList.remove('active')
-        }, reset);
-    })
-})
+gsap.set(".img", {
+    top: "45%",
+    left: "50%",
+    transform: "translate(-50%, -50%) scale(0)",
 
-Shery.imageMasker(".img-container" /* Element to target.*/, {
-    //Parameters are optional.
-    // mouseFollower: true,
-    y: -100,
-    y: 50,
-    text: "Move",
-    ease: "cubic-bezier(0.23, 1, 0.320, 8)",
+});
+
+gsap.from('p',{
+    y: 40,
+    ease: "power4.inOut",
     duration: 1,
-  });
+    stagger: {
+        amount: 0.15,
+    },
+    delay: 0.5,
+});
 
-  Shery.textAnimate("h1" /* Element to target.*/, {
+
+gsap.to(".img",{
+    scale: 1,
+    width: "300px",
+    height: "400px",
+    stagger: 0.15,
+    duration: 0.75,
+    ease: "power2.out",
+    delay: 1,
+    onComplete: scatterAndShrink,
+});
+
+
+gsap.to("p", {
+    top: "40px",
+    ease: "power4.inOut",
+    duration: 1,
+    stagger: {
+        amount: 0.15
+    },
+    delay: 3,
+    onComplete: () => {
+        document.querySelector("header").remove()
+    },
+});
+
+
+gsap.from("a", {
+    y: 20,
+    opacity: 0,
+    ease: "power2.out",
+    duration: 1,
+    stagger: {
+        amount: 0.15
+    },
+    delay: 4,
+});
+
+
+function scatterAndShrink(){
+    gsap.to(".img", {
+        top: (i) => position[i].top,
+        left: (i) => position[i].left,
+        transform: "none",
+        width: "75px",
+        height: "100px",
+        stagger: 0.075,
+        duration: 0.75,
+        ease: "power2.out"
+    })
+}
+
+Shery.textAnimate("p" /* Element to target.*/, {
     //Parameters are optional.
     style: 1,
     y: 10,
     delay: 0.1,
-    duration: 0.2,
+    duration: 1,
     ease: "cubic-bezier(0.23, 1, 0.320, 1)",
-    // multiplier: 0.1,
+    multiplier: 0.1,
   });
 
-Shery.makeMagnet(".img-container" /* Element to target.*/, {
+  Shery.mouseFollower({
     //Parameters are optional.
-    ease: "cubic-bezier(0.23, 1, 0.320, 2)",
+    skew: true,
+    ease: "cubic-bezier(0.23, 1, 0.320, 1)",
     duration: 1,
-});
+  });
+
 
 
