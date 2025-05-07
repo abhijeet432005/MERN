79c4db226abbda51b2151ff248b9f3baca5231d0
abhijeet432005 <abhijeet42kumar@gmail.com>
2025-05-07 21:32:34 +0530

    .

--- DOM Manipulation/gsap/instagram stories animation/script.js
+++ DOM Manipulation/gsap/instagram stories animation/script.js
@@ -1,28 +1,110 @@
-const lenis = new Lenis()
-lenis.on('scroll', ScrollTrigger.update)
+const lenis = new Lenis();
+lenis.on('scroll', ScrollTrigger.update);
 gsap.ticker.add((time) => {
- lenis.raf(time * 1000)
-})
-gsap.ticker.lagSmoothing(0)
+  lenis.raf(time * 1000);
+});
+gsap.ticker.lagSmoothing(0);
 
-const scene = new THREE.Scrne();
-scene.background = new THREE.color(0xfedfd);
 
-const camera = new THREE.PrespectiveCamera(
-    75, 
-    window.innerWidth / window.innerHeight,
-    0.1,
-    1000
-)
+const scene = new THREE.Scene();
+scene.background = new THREE.Color(0xfefdfd);
+
+
+const camera = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
+);
+
 
 const renderer = new THREE.WebGLRenderer({
-    antialise: true,
-    alpha: true,
-
-})
+  antialias: true,
+  alpha: true,
+});
 
 renderer.setClearColor(0xffffff, 1);
 renderer.setSize(window.innerWidth, window.innerHeight);
-renderer.SetPixelRario(window.devicePixelRatio);
-renderer.shaodowMap.enable = true;
-renderer.shaodowMap.type = THREE.PCFSoftShadowMap;
+renderer.setPixelRatio(window.devicePixelRatio);
+renderer.shadowMap.enabled = true;
+renderer.shadowMap.type = THREE.PCFSoftShadowMap;
+renderer.physicallyCorrectLights = true;
+renderer.toneMapping = THREE.ACESFilmicToneMapping;
+renderer.toneMappingExposure = 2.5;
+document.querySelector('.model').appendChild(renderer.domElement);
+
+
+
+const ambientLight = new THREE.AmbientLight(0xffffff, 3);
+scene.add(ambientLight);
+
+const mainlight = new THREE.DirectionalLight(0xffffff, 1);
+mainlight.position.set(5, 10, 7.5);
+scene.add(mainlight);
+
+
+const fillLight = new THREE.DirectionalLight(0xffffff, 3);
+fillLight.position.set(-5, 0, -5);
+scene.add(fillLight);
+
+
+const hemiLight = new THREE.HemisphereLight(0xffffff, 0xffffff, 2);
+hemiLight.position.set(0, 25, 0);
+scene.add(hemiLight);
+
+
+
+function basicAnimate() {
+  renderer.render(scene, camera);
+  requestAnimationFrame(basicAnimate);
+}
+
+basicAnimate();
+
+let model;
+const loader = new THREE.GLTFLoader()
+
+
+loader.load("./asset/pepsi.glb", function (gltf) {
+  model = gltf.scene;
+
+  model.traverse((node) => {
+    if (node.isMesh && node.material) {
+      node.material.metalness = 0.4;
+      node.material.roughness = 0.4;
+      node.material.envMapIntensity = 1.5;
+      node.castShadow = true;
+      node.receiveShadow = true;
+    }
+  });
+
+  const box = new THREE.Box3().setFromObject(model);
+  const center = box.getCenter(new THREE.Vector3());
+  model.position.sub(center);
+  scene.add(model);
+
+  const size = box.getSize(new THREE.Vector3());
+  const maxDim = Math.max(size.x, size.y, size.z);
+
+  camera.position.z = maxDim * 1.5;
+  
+  model.scale.set(0,0,0)
+  playInitialAnimation();
+
+  cancelAnimationFrame(basicAnimate);
+  animate();
+});
+
+
+const floatAmplitude = 0.2;
+const floatSpeed = 1.5;
+const rotationSpeed = 0.3;
+let isFloating = true;
+let currentScroll = 0;
+
+
+const stickyHeight = window.innerHeight;
+const scannerSection = document.querySelector(".scanner");
+const scannerPosition = scannerSection.offsetTop;
+const scanContainer = document.querySelector(".scan-cont");
+gsap.set(scanContainer, {scale: 0});
