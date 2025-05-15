bf2339c042514f63a86954f5c6e7b2166edf2564
abhijeet432005 <abhijeet42kumar@gmail.com>
2025-05-15 16:42:06 +0530

    .

--- DOM Manipulation/gsap/New folder/script.js
+++ DOM Manipulation/gsap/New folder/script.js
@@ -1,115 +1,145 @@
+// import Lenis from "lenis";
+// import gsap from "gsap";
+// import { ScrollTrigger } from "gsap/all";
+
+gsap.registerPlugin(ScrollTrigger);
+
 document.addEventListener("DOMContentLoaded", () => {
-    const imageSources = [
-        "./asset/img27.jpg",
-        "./asset/img7.jpg",
-        "./asset/img28.jpg",
-        "./asset/img24.jpg",
-        "./asset/img3.jpg",
-        
-    ]
+  const lenis = new Lenis();
 
-    const menuItems = document.querySelectorAll(".menu-item");
+  lenis.on("scroll", ScrollTrigger.update);
 
-    menuItems.forEach((item) => {
-        const copyElement = item.querySelectorAll(".info, .name, .tag");
+  gsap.ticker.add((time) => {
+    lenis.raf(time * 1000);
+  });
 
-        copyElement.forEach((div) => {
-            const copy = div.querySelector("p");
+  gsap.ticker.lagSmoothing(0);
 
-            if(copy){
-                const duplicateCopy = document.createElement("p");
-                duplicateCopy.textContent = copy.textContent;
-                div.appendChild(duplicateCopy)
+  
+  document.querySelectorAll(".img:not([data-origin])").forEach((img, index) => {
+    img.setAttribute("data-origin", index % 2 === 0 ? "left" : "right");
+  });
 
+  gsap.set(".img", {
+    scale: 0,
+    force3D: true,
+  });
+
+  const rows = document.querySelectorAll(".row");
+
+  rows.forEach((row, index) => {
+    const rowImages = row.querySelectorAll(".img");
+
+    if (rowImages.length > 0) {
+      row.id = `row-${index}`;
+
+      ScrollTrigger.create({
+        id: `scaleIn-${index}`,
+        trigger: row,
+        start: "top bottom",
+        end: "bottom bottom-=10%",
+        scrub: 1,
+        invalidateOnRefresh: true,
+
+        onUpdate: function (self) {
+          if (self.isActive) {
+            const progress = self.progress;
+            const easedProgress = Math.min(1, progress * 1.2);
+            const scaleValue = gsap.utils.interpolate(0, 1, easedProgress);
+
+            rowImages.forEach((img) => {
+              gsap.set(img, {
+                scale: scaleValue,
+                force3D: true,
+              });
+            });
+
+            if (progress > 0.95) {
+              gsap.set(rowImages, {
+                scale: 1,
+                force3D: true,
+              });
             }
-        })
-    })
+          }
+        },
 
-    const appendImages = (src) => {
-        const preview1 = document.querySelector(".preview-img-1")
-        const preview2 = document.querySelector(".preview-img-2")
+        onLeave: function () {
+          gsap.set(rowImages, {
+            scale: 1,
+            force3D: true,
+          });
+        },
+      });
 
-        const img1 = document.createElement("img")
-        const img2 = document.createElement("img")
+      ScrollTrigger.create({
+        id: `scaleOut-${index}`,
+        trigger: row,
+        start: "top top",
+        end: "bottom top",
+        pin: true,
+        pinSpacing: false,
+        scrub: 1,
+        invalidateOnRefresh: true,
+        onEnter: () => {
+            gsap.to(rowImages, {scale: 1, force3D: true});
+        },
+        onUpdate: (self) => {
+            if(self.isActive){
+                const scale = gsap.utils.interpolate(1, 0, self.progress);
 
-        img1.src = src;
-        img1.style.clipPath = "polygon(0% 100%, 100% 100%, 100% 100%, 0% 100%)";
-
-        img2.src = src;
-        img2.style.clipPath = "polygon(0% 100%, 100% 100%, 100% 100%, 0% 100%)";
-
-        preview1.appendChild(img1)
-        preview2.appendChild(img2)
-
-        gsap.to([img1, img2], {
-            clipPath: "polygon(0% 100%, 100% 100%, 100% 0%, 0% 0%)",
-            duration: 1,
-            ease: "power3.out",
-            onComplete: function () {
-                removeExtraImages(preview1)
-                removeExtraImages(preview2)
+                rowImages.forEach((img) => {
+                    gsap.set(img, {
+                        scale: scale,
+                        force3D: true,
+                        clearProps: self.progress === 1 ? "scale" : "",
+                    });
+                });
+            } else {
+                const isAbove = self.scroll() < self.start;
+                if(isAbove){
+                    gsap.set(rowImages, {
+                        scale: 1,
+                        force3D: true
+                    })
+                }
             }
-        })
-
-    }
-
-    function removeExtraImages(container){
-        while(container.children.length > 10){
-            container.removeChild(container.firstChild)
         }
+      })
+
+      ScrollTrigger.create({
+        id: `marker-${index}`,
+        trigger: row,
+        start: "bottom bottom",
+        end: "top top",
+        onEnter: () => {
+            const scaleOut = ScrollTrigger.getById(`scaleOut-${index}`);
+            if(scaleOut && scaleOut.progress === 0){
+                gsap.to(rowImages, {scale: 1, force3D: true})
+            }
+        },
+
+        onLeave: () => {
+            const scaleOut = ScrollTrigger.getById(`scaleOut-${index}`);
+            if(scaleOut && scaleOut.progress === 0){
+                gsap.to(rowImages, {scale: 1, force3D: true})
+            }
+        },
+
+        onEnterBack: () => {
+            const scaleOut = ScrollTrigger.getById(`scaleOut-${index}`);
+            if(scaleOut && scaleOut.progress === 0){
+                gsap.to(rowImages, {scale: 1, force3D: true})
+            }
+        }
+      })
+
     }
+  });
 
-    document.querySelectorAll(".menu-item").forEach((item, index) => {
-        item.addEventListener("mouseover", () => {
-            mouseOverAnimation(item)
-            appendImages(imageSources[index]);
+  window.addEventListener("resize", () => {
+    ScrollTrigger.refresh(true)
+  })
+});
 
-        });
 
-        item.addEventListener("mouseout", () => {
-            mouseOutAnimation(item)
-        })
-    })
 
-    const mouseOverAnimation = (elem) => {
-        gsap.to(elem.querySelectorAll("p:nth-child(1)"), {
-            top: "-100%",
-            duration: 0.3,
-        });
-        gsap.to(elem.querySelectorAll("p:nth-child(2)"), {
-            top: "0%",
-            duration: 0.3,
-        });
-        
-    }
-    const mouseOutAnimation = (elem) => {
-        gsap.to(elem.querySelectorAll("p:nth-child(1)"), {
-            top: "0%",
-            duration: 0.3,
-        });
-        gsap.to(elem.querySelectorAll("p:nth-child(2)"), {
-            top: "100%",
-            duration: 0.3,
-        });
-        
-    }
-
-    document.querySelector(".menu").addEventListener("mouseout", () => {
-        gsap.to(".preview-img img", {
-            clipPath: "polygon(0% 0%, 100% 0%, 100% 0%, 0% 0%)",
-            duration: 1,
-            ease: "power3.out",
-        })
-    })
-
-    document.addEventListener("mousemove", (e) => {
-        const preview = document.querySelector(".preview");
-
-        gsap.to(preview, {
-            x: e.clientX,
-            y: e.clientY,
-            duration: 1,
-            ease: "power3.out"
-        })
-    })
-})
