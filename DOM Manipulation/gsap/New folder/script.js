0688857b08ebbbdde98ba590ea32ec30d5d2bd35
abhijeet432005 <abhijeet42kumar@gmail.com>
2025-05-16 19:35:56 +0530

    .

--- DOM Manipulation/gsap/New folder/script.js
+++ DOM Manipulation/gsap/New folder/script.js
@@ -1,145 +1,62 @@
-// import Lenis from "lenis";
-// import gsap from "gsap";
-// import { ScrollTrigger } from "gsap/all";
-
-gsap.registerPlugin(ScrollTrigger);
-
 document.addEventListener("DOMContentLoaded", () => {
-  const lenis = new Lenis();
+    const lenis = new Lenis();
+        
+    lenis.on('scroll', ScrollTrigger.update);
+    gsap.ticker.add((time) => {
+        lenis.raf(time * 1000);
+    });
 
-  lenis.on("scroll", ScrollTrigger.update);
+    gsap.ticker.lagSmoothing(0);
 
-  gsap.ticker.add((time) => {
-    lenis.raf(time * 1000);
-  });
+    const cards = gsap.utils.toArray(".card")
+    const rotation = [-12,10,-5,5,-5,2];
 
-  gsap.ticker.lagSmoothing(0);
+    cards.forEach((card, index) => {
+        gsap.set(card, {
+            y: window.innerHeight,
+            rotate: rotation[index]
+        })
+    })
 
-  
-  document.querySelectorAll(".img:not([data-origin])").forEach((img, index) => {
-    img.setAttribute("data-origin", index % 2 === 0 ? "left" : "right");
-  });
-
-  gsap.set(".img", {
-    scale: 0,
-    force3D: true,
-  });
-
-  const rows = document.querySelectorAll(".row");
-
-  rows.forEach((row, index) => {
-    const rowImages = row.querySelectorAll(".img");
-
-    if (rowImages.length > 0) {
-      row.id = `row-${index}`;
-
-      ScrollTrigger.create({
-        id: `scaleIn-${index}`,
-        trigger: row,
-        start: "top bottom",
-        end: "bottom bottom-=10%",
-        scrub: 1,
-        invalidateOnRefresh: true,
-
-        onUpdate: function (self) {
-          if (self.isActive) {
-            const progress = self.progress;
-            const easedProgress = Math.min(1, progress * 1.2);
-            const scaleValue = gsap.utils.interpolate(0, 1, easedProgress);
-
-            rowImages.forEach((img) => {
-              gsap.set(img, {
-                scale: scaleValue,
-                force3D: true,
-              });
-            });
-
-            if (progress > 0.95) {
-              gsap.set(rowImages, {
-                scale: 1,
-                force3D: true,
-              });
-            }
-          }
-        },
-
-        onLeave: function () {
-          gsap.set(rowImages, {
-            scale: 1,
-            force3D: true,
-          });
-        },
-      });
-
-      ScrollTrigger.create({
-        id: `scaleOut-${index}`,
-        trigger: row,
+    ScrollTrigger.create({
+        trigger: ".sticky-card",
         start: "top top",
-        end: "bottom top",
+        end: `+=${window.innerHeight * 8}px`,
         pin: true,
-        pinSpacing: false,
+        pinSpacing: true,
         scrub: 1,
-        invalidateOnRefresh: true,
-        onEnter: () => {
-            gsap.to(rowImages, {scale: 1, force3D: true});
-        },
         onUpdate: (self) => {
-            if(self.isActive){
-                const scale = gsap.utils.interpolate(1, 0, self.progress);
+            const progress = self.progress;
+            const totalCards = cards.length;
+            const progressPerCard = 1 / totalCards;
 
-                rowImages.forEach((img) => {
-                    gsap.set(img, {
-                        scale: scale,
-                        force3D: true,
-                        clearProps: self.progress === 1 ? "scale" : "",
-                    });
-                });
-            } else {
-                const isAbove = self.scroll() < self.start;
-                if(isAbove){
-                    gsap.set(rowImages, {
-                        scale: 1,
-                        force3D: true
-                    })
+            cards.forEach((card, index) => {
+                const cardStart = index * progressPerCard;
+                let cardProgress = (progress - cardStart) / progressPerCard;
+                cardProgress = Math.min(Math.max(cardProgress, 0), 1);
+
+                let yPos = window.innerHeight * (1 - cardProgress);
+                let xPos = 0;
+
+                if(cardProgress === 1 && index < totalCards - 1){
+                    const remaningProgress  = (progress - (cardStart + progressPerCard)) / (1 - (cardStart + progressPerCard));
+
+                    if(remaningProgress > 0){
+                        const distanceMultiplier = 1 - index * 0.15;
+                        xPos = -window.innerWidth * 0.3 * distanceMultiplier * remaningProgress;
+
+                        yPos = -window.innerHeight * 0.3 * distanceMultiplier * remaningProgress;
+
+                    }
                 }
-            }
+
+                gsap.to(card, {
+                    y: yPos,
+                    x: xPos,
+                    duration: 0,
+                    ease: "none"
+                })
+            })
         }
-      })
-
-      ScrollTrigger.create({
-        id: `marker-${index}`,
-        trigger: row,
-        start: "bottom bottom",
-        end: "top top",
-        onEnter: () => {
-            const scaleOut = ScrollTrigger.getById(`scaleOut-${index}`);
-            if(scaleOut && scaleOut.progress === 0){
-                gsap.to(rowImages, {scale: 1, force3D: true})
-            }
-        },
-
-        onLeave: () => {
-            const scaleOut = ScrollTrigger.getById(`scaleOut-${index}`);
-            if(scaleOut && scaleOut.progress === 0){
-                gsap.to(rowImages, {scale: 1, force3D: true})
-            }
-        },
-
-        onEnterBack: () => {
-            const scaleOut = ScrollTrigger.getById(`scaleOut-${index}`);
-            if(scaleOut && scaleOut.progress === 0){
-                gsap.to(rowImages, {scale: 1, force3D: true})
-            }
-        }
-      })
-
-    }
-  });
-
-  window.addEventListener("resize", () => {
-    ScrollTrigger.refresh(true)
-  })
-});
-
-
-
+    })
+})
