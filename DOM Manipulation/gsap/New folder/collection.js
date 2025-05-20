21d0487c479afdf618f4275c9f9bd1c5e788ee96
abhijeet432005 <abhijeet42kumar@gmail.com>
2025-05-20 17:54:32 +0530

    .

---
+++ DOM Manipulation/gsap/New folder/collection.js
@@ -0,0 +1,25 @@
+const collection = [
+  { title: "Shadow Profile", img: "./asset/img10.jpg" },
+  { title: "Digital Mirage", img: "./asset/img11.jpg" },
+  { title: "Echoes of Light", img: "./asset/img12.jpg" },
+  { title: "Neon Depth", img: "./asset/img13.jpg" },
+  { title: "Fractured Reality", img: "asset/img14.jpg" },
+  { title: "Urban Dusk", img: "./asset/img15.jpg" },
+  { title: "Pixel Pulse", img: "./asset/img16.jpg" },
+  { title: "Glitch Vibe", img: "./asset/img17.jpg" },
+  { title: "Dream Sequence", img: "./asset/img18.jpg" },
+  { title: "Faded Vision", img: "./asset/img19.jpg" },
+  { title: "Quantum Drift", img: "./asset/img20.jpg" },
+  { title: "Virtual Trance", img: "./asset/img21.jpg" },
+  { title: "Retro Stream", img: "./asset/img22.jpg" },
+  { title: "Electric Silence", img: "./asset/img23.jpg" },
+  { title: "Lucid Static", img: "./asset/img24.jpg" },
+  { title: "Silent Utopia", img: "./asset/img25.jpg" },
+  { title: "Cyber Fade", img: "./asset/img26.jpg" },
+  { title: "Ambient Code", img: "./asset/img27.jpg" },
+  { title: "Digital Bloom", img: "./asset/img28.jpg" },
+  { title: "Phantom Flow", img: "./asset/img29.jpg" },
+  { title: "Parallel Echo", img: "./asset/img30.jpg" }
+];
+
+export default collection;
