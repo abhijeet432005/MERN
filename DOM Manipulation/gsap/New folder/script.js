41f0300d474093008219a3c8473f8f2f99a66e70
abhijeet432005 <abhijeet42kumar@gmail.com>
2025-05-28 21:41:57 +0530

    .

--- DOM Manipulation/gsap/New folder/script.js
+++ DOM Manipulation/gsap/New folder/script.js
@@ -1,107 +1,26 @@
-window.onload = function () {
-    const lenis = new Lenis();
+const scroller = new LocomotiveScroll({
+    el: document.querySelector("[data-scroll-container]"),
+    smooth: true
+})
 
-    lenis.on('scroll', ScrollTrigger.update);
+gsap.registerPlugin(Flip)
 
-    gsap.ticker.add((time) => {
-        lenis.raf(time * 1000);
-    });
+CustomEase.create("cubic", "0.83, 0, 0.17, 1")
+const gallery = document.querySelector(".img-gallery-container"),
+images = gsap.utils.toArray(".img");
 
-    gsap.ticker.lagSmoothing(0);
-    gsap.registerPlugin(ScrollTrigger);
+document.querySelector(".btn").addEventListener("click", () => {
+    let state = Flip.getState(".img-gallery-container .img")
 
-    const gallery = document.querySelector(".gallery")
-    console.log(gallery)
-    const previewImg = document.querySelector(".preview-img img")
-
-    document.addEventListener("mousemove", function (event) {
-        console.log("Triggering animation");
-        const x = event.clientX;
-        const y = event.clientY;
-
-        const centerX = window.innerWidth / 2;
-        const centerY = window.innerHeight / 2;
-
-        const percentX = (x - centerX) / centerX;
-        const percentY = (y - centerY) / centerY;
-
-        const rotateX = 55 + percentY * 2;
-        const rotateY = percentX * 2;
-
-
-        gsap.to(".gallery", {
-            duration: 1,
-            ease: "power2.out",
-            transform: `translateX(-50%) rotateX(${rotateX}deg) rotateY(${rotateY}deg)`,
-            overwrite: "auto",
-        });
+    gallery.classList.toggle("order")
+    images.forEach((img, i) => {
+        img.classList.toggle("reorder")
     })
 
-    for (let i = 0; i < 200; i++) {
-        const item = document.createElement("div")
-        item.className = "item"
-        const img = document.createElement("img")
-        img.src = `./asset/img${(i % 15) + 1}.jpg`
-        item.appendChild(img)
-        gallery.appendChild(item)
-    }
-
-    const items = document.querySelectorAll(".item")
-    const numberOfItem = items.length;
-    const angleIncrement = 360 / numberOfItem;
-
-    items.forEach((item, index) => {
-        gsap.set(item, {
-            rotationY: 90,
-            rotationZ: index * angleIncrement - 90,
-            transformOrigin: "50% 400px",
-        })
-
-        item.addEventListener("mouseover", () => {
-            const imgInsideItem = item.querySelector("img")
-            previewImg.src = imgInsideItem.src;
-
-            gsap.to(item, {
-                x: 10,
-                y: 10,
-                z: 10,
-                ease: "power2.out",
-                duration: 0.5,
-            })
-        })
-
-        item.addEventListener("mouseout", () => {
-            previewImg.src = "./asset/img12.jpg"
-            gsap.to(item, {
-                x: 10,
-                y: 10,
-                z: 10,
-                ease: "power2.out",
-                duration: 0.5,
-            })
-        })
+    Flip.from(state, {
+        absolute: true,
+        duration: 2, 
+        stagger: 0.05,
+        ease: "cubic"
     })
-
-    ScrollTrigger.create({
-        trigger: "body",
-        start: "top top",
-        end: "bottom bottom",
-        scrub: 2,
-        onRefresh: setupRotation,
-        onUpdate: (self) => {
-            // console.log("Scroll progress:", self.progress);
-            const rotationProgress = self.progress * 360 + 1;
-            items.forEach((item, index) => {
-                const currentAngle = index * angleIncrement - 90 + rotationProgress;
-                gsap.to(item, {
-                    rotationZ: currentAngle,
-                    duration: 1,
-                    ease: "power3.out",
-                    overwrite: "auto",
-                })
-            })
-        }
-    })
-}
-
-function setupRotation() { }
+})
