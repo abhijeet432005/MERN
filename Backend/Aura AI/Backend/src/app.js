fd51f87567ee065a3e696864dd74dcdbf488df44
Abhijeet kumar <abhijeetkumar@Abhijeets-MacBook-Air.local>
2026-07-20 15:51:24 +0530

    update

--- Backend/Aura AI/Backend/src/app.js
+++ Backend/Aura AI/Backend/src/app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const ConnectToDB = require('./db/db')
 const cookieParser = require('cookie-parser')
 const cors = require('cors')
+const path = require('path')
 
 // Routes 
 const authRoute = require('./routes/auth.route')
@@ -19,6 +20,7 @@ app.use(cors({
 }))
 app.use(cookieParser())
 app.use(express.json())
+app.use(express.static(path.join(__dirname, '../public')))
 
 
 // Using Routes 
@@ -26,5 +28,10 @@ app.use(express.json())
 app.use("/api/auth", authRoute)
 app.use("/api/chat", chatRoute)
 app.use("/api/message", messageRoute)
+
+
+app.get("*name", (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/index.html'))
+})
 
 module.exports = app
